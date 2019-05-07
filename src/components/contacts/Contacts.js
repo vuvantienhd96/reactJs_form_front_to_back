e1ce54e5c597bbbc8324e2a6d7daa4e9ff8abf25
tienvuv <congtuocxauxa000@gmail.com>
2019-05-07 10:37:15 +0100

    feat-jst- fix path

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./contact/Contact";
+import Contact from "./Contact";
 import { Consumer } from "../context";
 
 class Contacts extends Component {
